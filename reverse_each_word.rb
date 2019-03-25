b1c52578957a03606cf5c6a1b9304d73b8c45b73
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-25 15:54:32 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,26 @@
+
+=begin
+# Reverses all words in a string without reversing the order of the words
+def reverse_each_word(string)
+  string_array = string.split # Convert string to array for iteration
+  return_array = [ ]          # Set container array
+  
+  # Reverse each word and insert into array
+  string_array.each { |word| return_array << word.reverse }
+  
+  return_array.join(" ")      # Convert back into string for display
+end
+=end
+
+# Reverses all words in a string using collect
+def reverse_each_word(string)
+  string_array = string.split # Convert string to array for iteration
+  return_string = "" # Set container string
+
+  # Reverse each word and place into new array
+  string_array.collect { |word| return_string << word.reverse }
+  
+end
+
+puts reverse_each_word("Hello there, and how are you?")
+puts "Should return: 'olleH ,ereht dna woh era ?uoy'"
